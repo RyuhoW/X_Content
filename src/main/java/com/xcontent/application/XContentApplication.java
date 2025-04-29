6a633c02617261b8d748c88eb39208006235b9aa
Ryuho <ryuho.w0610@gmail.com>
2025-04-29 23:42:13 +0900

    Initial project setup: - Create Maven project structure - Configure dependencies in pom.xml - Set up basic directory structure - Add .gitignore file - Create main application class - Configure logging

---
+++ src/main/java/com/xcontent/application/XContentApplication.java
@@ -0,0 +1,32 @@
+package com.xcontent.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XContentApplication extends Application {
+    private static final Logger logger = LoggerFactory.getLogger(XContentApplication.class);
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            primaryStage.setTitle("X.com Content Generator");
+            primaryStage.show();
+            
+            logger.info("Application started successfully");
+        } catch (Exception e) {
+            logger.error("Failed to start application", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void stop() {
+        logger.info("Application shutting down");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
